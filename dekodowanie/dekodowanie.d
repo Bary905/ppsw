b8c7204627742bc69f49ae1e6a4f58fd059db1df
Bary <switalskibartlomiej@gmail.com>
2025-05-21 00:13:09 +0200

    .

--- dekodowanie/dekodowanie.d
+++
@@ -1,4 +0,0 @@
-.\dekodowanie.o: dekodowanie.c
-.\dekodowanie.o: C:\Keil\ARM\Inc\Philips\LPC21xx.H
-.\dekodowanie.o: konwersje.h
-.\dekodowanie.o: lancuchy.h
--- dekodowanie/konwersje.d
+++
@@ -1,3 +0,0 @@
-.\konwersje.o: konwersje.c
-.\konwersje.o: C:\Keil\ARM\Inc\Philips\LPC21xx.H
-.\konwersje.o: konwersje.h
